a7d30e3b0aa7d8f29bcb21a406feb68e8ea977e6
Freelf <zdp930128@gmail.com>
2021-02-05 15:30:43 +0800

    feat: add remove linked list element answer

--- ConquerAlgorithm/LinkList/RemoveLinkListElement.swift
+++ ConquerAlgorithm/LinkList/RemoveLinkListElement.swift
@@ -12,6 +12,52 @@
 import Foundation
 class RemoveLinkListElementsSolution {
     func removeElements(_ head: ListNode?, _ val: Int) -> ListNode? {
-
+        if head == nil {
+            return nil
+        }
+        head?.next = removeElements(head?.next, val)
+        if head?.val == val {
+            return head?.next
+        }
+        return head
+    }
+    func removeElementsOne(_ head: ListNode?, _ val: Int) -> ListNode? {
+        
+        if head == nil {
+            return nil
+        }
+        var newHead = head
+        while newHead?.val == val {
+            newHead = newHead?.next
+        }
+        if newHead == nil {
+            return nil
+        }
+        var prev = newHead
+        while prev?.next != nil {
+            if prev?.next?.val == val {
+                prev?.next = prev?.next?.next
+            } else {
+                prev = prev?.next
+            }
+        }
+        return newHead
+    }
+    
+    func removeElementsTwo(_ head: ListNode?, _ val: Int) -> ListNode? {
+        if head == nil {
+            return nil
+        }
+        let dummyNode = ListNode(val - 1)
+        dummyNode.next = head
+        var prev: ListNode? = dummyNode
+        while prev?.next != nil {
+            if prev?.next?.val == val {
+                prev?.next = prev?.next?.next
+            } else {
+                prev = prev?.next
+            }
+        }
+        return dummyNode.next
     }
 }
